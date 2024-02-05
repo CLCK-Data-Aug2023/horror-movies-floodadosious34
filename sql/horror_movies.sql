2019efa5efd72b7bec96f4d692891c5055ad2ea7
James Flood Jr <floodadosious111@gmail.com>
2024-02-05 14:55:17 -0500

    added code

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,18 @@
 -- Add your SQL here
+-- SELECT * FROM movies;
+-- ALTER TABLE movies
+-- CHANGE COLUMN id Movie_ID VARCHAR(255);
+-- RENAME COLUMN name TO Movie_Title VARCHAR(255); 
+-- RENAME COLUMN INT TO Rating;
+
+WITH cte AS (
+    SELECT Movie_ID, Movie_Title, Rating,
+        DENSE_RANK() OVER (ORDER BY Rating DESC) dr
+    FROM movies
+    WHERE genre = 'horror' 
+    AND year <= 1985  
+)
+SELECT Movie_ID, Movie_Title, Rating
+FROM cte
+WHERE dr <= 3 
+ORDER BY Rating DESC;
